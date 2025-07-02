899e21eddd97ce14a329f05f873af9de2592968b
FaheemGurkani <faheemGurkani@gmail.com>
2025-07-03 04:25:53 +0500

    Third Commit

--- src/client.py
+++ src/client.py
@@ -1,230 +1,303 @@
-# import asyncio
-# import json
-# import traceback
-# from contextlib import AsyncExitStack
-# from typing import Any, Dict, List, Optional
-# from pathlib import Path
+import asyncio
+import json
+import traceback
+from contextlib import AsyncExitStack
+from typing import Any, Dict, List, Optional
+from pathlib import Path
 
-# import nest_asyncio
-# from dotenv import load_dotenv
-# from mcp import ClientSession, StdioServerParameters
-# from mcp.client.stdio import stdio_client
-# from openai import AsyncOpenAI
+import nest_asyncio
+from dotenv import load_dotenv
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+from openai import AsyncOpenAI
+import sys
 
 
 
-# nest_asyncio.apply()
+nest_asyncio.apply()
 
-# # Loading .env from project root
-# load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent / ".env")
+# Loading .env from project root
+load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent / ".env")
 
-# class MCPOpenAIClient:
+class MCPOpenAIClient:
 
-#     def __init__(self, model: str = "gpt-4o"):
-#         self.session: Optional[ClientSession] = None
-#         self.exit_stack = AsyncExitStack()
-#         self.openai_client = AsyncOpenAI()
-#         self.model = model
-#         self.stdio: Optional[Any] = None
-#         self.write: Optional[Any] = None
+    def __init__(self, model: str = "gpt-4o"):
+        self.session: Optional[ClientSession] = None
+        self.exit_stack = AsyncExitStack()
+        self.openai_client = AsyncOpenAI()
+        self.model = model
+        self.stdio: Optional[Any] = None
+        self.write: Optional[Any] = None
 
-#     async def connect_to_server(self, server_script_path: str = "server.py"):
+    async def connect_to_server(self, server_script_path: str = "server.py"):
         
-#         try:
-#             server_params = StdioServerParameters(
-#                 command="python",
-#                 args=[server_script_path],
-#                 stderr_to_stdout=True
-#             )
+        try:
+            server_params = StdioServerParameters(
+                command="python",
+                args=[server_script_path],
+                stderr_to_stdout=True
+            )
 
-#             print("[DEBUG] Launching server subprocess...")
-#             stdio_transport = await self.exit_stack.enter_async_context(
-#                 stdio_client(server_params)
-#             )
+            print("[DEBUG] Launching server subprocess...")
+            stdio_transport = await self.exit_stack.enter_async_context(
+                stdio_client(server_params)
+            )
 
-#             # await asyncio.sleep(35)   # For, testing
+            # await asyncio.sleep(35)   # For, testing
 
-#             print("[DEBUG] Server subprocess launched successfully.")
+            print("[DEBUG] Server subprocess launched successfully.")
 
-#             self.stdio, self.write = stdio_transport
+            self.stdio, self.write = stdio_transport
 
-#             # print(self.stdio)   # For, testing
-#             # print(self.write)
+            # print(self.stdio)   # For, testing
+            # print(self.write)
 
-#             self.session = await self.exit_stack.enter_async_context(
-#                 ClientSession(self.stdio, self.write)
-#             )
+            self.session = await self.exit_stack.enter_async_context(
+                ClientSession(self.stdio, self.write)
+            )
 
-#             print("Waiting for MCP server to be ready...")
+            print("Waiting for MCP server to be ready...")
 
-#             # Try initializing with retries (e.g., for up to 5 seconds)
-#             for attempt in range(15):
+            # Try initializing with retries (e.g., for up to 5 seconds)
+            for attempt in range(15):
                 
-#                 try:
-#                     await self.session.initialize()
+                try:
+                    await self.session.initialize()
                 
-#                     print("Session Initialized")
+                    print("Session Initialized")
                 
-#                     break
+                    break
                 
-#                 except Exception as init_err:
-#                     print(f"Initialization attempt {attempt + 1} failed: {init_err}")
+                except Exception as init_err:
+                    print(f"Initialization attempt {attempt + 1} failed: {init_err}")
                 
-#                     await asyncio.sleep(5)
-#             else:
-#                 raise RuntimeError("MCP server failed to initialize after retries.")
+                    await asyncio.sleep(5)
+            else:
+                raise RuntimeError("MCP server failed to initialize after retries.")
 
-#             tools_result = await self.session.list_tools()
+            tools_result = await self.session.list_tools()
 
-#             print("Connected to MCP server. Tools available:")
-#             for tool in tools_result.tools:
-#                 print(f"  - {tool.name}: {tool.description}")
+            print("Connected to MCP server. Tools available:")
+            for tool in tools_result.tools:
+                print(f"  - {tool.name}: {tool.description}")
 
-#         except Exception as e:
-#             print(f"Failed to connect or initialize session: {e}")
+        except Exception as e:
+            print(f"Failed to connect or initialize session: {e}")
             
-#             traceback.print_exc()
+            traceback.print_exc()
             
-#             await self.cleanup()
+            await self.cleanup()
             
-#             return
+            return
 
-#     async def get_mcp_tools(self) -> List[Dict[str, Any]]:
+    async def get_mcp_tools(self) -> List[Dict[str, Any]]:
         
-#         try:
-#             tools_result = await self.session.list_tools()
+        try:
+            tools_result = await self.session.list_tools()
         
-#             return [
-#                 {
-#                     "type": "function",
-#                     "function": {
-#                         "name": tool.name,
-#                         "description": tool.description,
-#                         "parameters": tool.inputSchema,
-#                     },
-#                 }
-#                 for tool in tools_result.tools
-#             ]
+            return [
+                {
+                    "type": "function",
+                    "function": {
+                        "name": tool.name,
+                        "description": tool.description,
+                        "parameters": tool.inputSchema,
+                    },
+                }
+                for tool in tools_result.tools
+            ]
         
-#         except Exception as e:
-#             print(f"Failed to fetch MCP tools: {e}")
+        except Exception as e:
+            print(f"Failed to fetch MCP tools: {e}")
         
-#             traceback.print_exc()
+            traceback.print_exc()
         
-#             return []
+            return []
 
-#     async def process_query(self, query: str) -> str:
+    # async def process_query(self, query: str) -> str:
         
-#         try:
-#             tools = await self.get_mcp_tools()
+    #     try:
+    #         tools = await self.get_mcp_tools()
         
-#             if not tools:
-#                 raise RuntimeError("No tools loaded from server.")
+    #         if not tools:
+    #             raise RuntimeError("No tools loaded from server.")
 
-#             response = await self.openai_client.chat.completions.create(
-#                 model=self.model,
-#                 messages=[{"role": "user", "content": query}],
-#                 tools=tools,
-#                 tool_choice="auto",
-#             )
+    #         response = await self.openai_client.chat.completions.create(
+    #             model=self.model,
+    #             messages=[{"role": "user", "content": query}],
+    #             tools=tools,
+    #             tool_choice="auto",
+    #         )
             
-#             assistant_message = response.choices[0].message
-#             messages = [{"role": "user", "content": query}, assistant_message]
+    #         assistant_message = response.choices[0].message
+    #         messages = [{"role": "user", "content": query}, assistant_message]
 
-#             # For, testing
-#             print("\nTool calls requested by OpenAI:")
-#             for tc in assistant_message.tool_calls:
-#                 print(f"🔹 {tc.function.name} with args: {tc.function.arguments}")
+    #         # For, testing
+    #         print("\nTool calls requested by OpenAI:")
+    #         for tc in assistant_message.tool_calls:
+    #             print(f"🔹 {tc.function.name} with args: {tc.function.arguments}")
 
-#             if assistant_message.tool_calls:
+    #         if assistant_message.tool_calls:
             
-#                 for tool_call in assistant_message.tool_calls:
-#                     result = await self.session.call_tool(
-#                         tool_call.function.name,
-#                         arguments=json.loads(tool_call.function.arguments),
-#                     )
+    #             for tool_call in assistant_message.tool_calls:
+    #                 result = await self.session.call_tool(
+    #                     tool_call.function.name,
+    #                     arguments=json.loads(tool_call.function.arguments),
+    #                 )
             
-#                     tool_result_dict = result.content[0].text
+    #                 tool_result_dict = result.content[0].text
 
-#                     # For, testing
-#                     print(f"\nTool '{tool_call.function.name}' returned:\n{json.dumps(tool_result_dict, indent=2)}")
+    #                 # For, testing
+    #                 print(f"\nTool '{tool_call.function.name}' returned:\n{json.dumps(tool_result_dict, indent=2)}")
 
-#                     messages.append({
-#                         "role": "tool",
-#                         "tool_call_id": tool_call.id,
-#                         "content": json.dumps(tool_result_dict),  
-#                     })
+    #                 messages.append({
+    #                     "role": "tool",
+    #                     "tool_call_id": tool_call.id,
+    #                     "content": json.dumps(tool_result_dict),  
+    #                 })
 
-#                 final_response = await self.openai_client.chat.completions.create(
-#                     model=self.model,
-#                     messages=messages,
-#                     tools=tools,
-#                     tool_choice="none",
-#                 )
+    #             final_response = await self.openai_client.chat.completions.create(
+    #                 model=self.model,
+    #                 messages=messages,
+    #                 tools=tools,
+    #                 tool_choice="none",
+    #             )
             
-#                 return final_response.choices[0].message.content
+    #             return final_response.choices[0].message.content
 
-#             return assistant_message.content
+    #         return assistant_message.content
 
-#         except Exception as e:
-#             print(f"Error during query processing: {e}")
+    #     except Exception as e:
+    #         print(f"Error during query processing: {e}")
             
-#             traceback.print_exc()
+    #         traceback.print_exc()
             
-#             return "Error occurred during query processing."
-
-#     # async def cleanup(self):
-#     #     try:
-#     #         await self.exit_stack.aclose()
+    #         return "Error occurred during query processing."
+    async def process_query(self, query: str) -> str:
         
-#     #     except Exception as e:
-#     #         print(f"Error during cleanup: {e}")
+        try:
+            tools = await self.get_mcp_tools()
         
-#     #         traceback.print_exc()
-#     async def cleanup(self):
+            if not tools:
+                raise RuntimeError("No tools loaded from server.")
 
-#         try:
+            # Step 1: Asking OpenAI to call redact_pii if needed
+            response = await self.openai_client.chat.completions.create(
+                model=self.model,
+                messages=[{"role": "user", "content": query}],
+                tools=tools,
+                tool_choice="auto",
+            )
+            assistant_message = response.choices[0].message
+            messages = [{"role": "user", "content": query}, assistant_message]
 
-#             if self.exit_stack:
-#                 await self.exit_stack.aclose()
+            pii_result = None
+            sanitized_result = None
 
-#             print("[INFO] Client and server cleaned up successfully.")
+            if assistant_message.tool_calls:
+        
+                for tool_call in assistant_message.tool_calls:
+        
+                    if tool_call.function.name == "redact_pii":
+                        redact_result = await self.session.call_tool(
+                            tool_call.function.name,
+                            arguments=json.loads(tool_call.function.arguments),
+                        )
+        
+                        redact_data = json.loads(redact_result.content[0].text)
 
-#             sys.exit(0)
+                        pii_result = redact_data
+        
+                        messages.append({
+                            "role": "tool",
+                            "tool_call_id": tool_call.id,
+                            "content": json.dumps(redact_data),
+                        })
 
-#         except Exception as e:
-#             print(f"[ERROR] Error during cleanup: {e}")
+                        # Step 2: Calling sanitize_prompt manually
+                        sanitize_args = {
+                            "redacted_text": redact_data["redacted_text"],
+                            "pii_meta": redact_data["pii_found"],
+                        }
+        
+                        sanitize_result = await self.session.call_tool(
+                            "sanitize_prompt", arguments=sanitize_args
+                        )
+        
+                        sanitized_result = sanitize_result.content[0].text
 
-#             traceback.print_exc()
+            # Step 3: Querying LLM with sanitized input if PII was found
+            final_query = sanitized_result if sanitized_result else query
+
+            print()
+            print(f"Redacted query: {final_query['sanitized_text']}")
+
+            final_response = await self.openai_client.chat.completions.create(
+                model=self.model,
+                messages=[{"role": "user", "content": final_query}],
+            )
+
+            return final_response.choices[0].message.content
+
+        except Exception as e:
+            print(f"❌ Error during query processing: {e}")
+            
+            traceback.print_exc()
+            
+            return "Error occurred during query processing."
+
+    # async def cleanup(self):
+    #     try:
+    #         await self.exit_stack.aclose()
+        
+    #     except Exception as e:
+    #         print(f"Error during cleanup: {e}")
+        
+    #         traceback.print_exc()
+    async def cleanup(self):
+
+        try:
+
+            if self.exit_stack:
+                await self.exit_stack.aclose()
+
+            print("[INFO] Client and server cleaned up successfully.")
+
+            return 
+
+        except Exception as e:
+            print(f"[ERROR] Error during cleanup: {e}")
+
+            traceback.print_exc()
 
 
 
-# async def main():
-#     client = MCPOpenAIClient()
+async def main():
+    client = MCPOpenAIClient()
     
-#     await client.connect_to_server("server.py")
+    await client.connect_to_server("server.py")
 
-#     if client.session is None:
-#         print("Client session was not established. Exiting.")
+    if client.session is None:
+        print("Client session was not established. Exiting.")
     
-#         return
+        return
 
-#     query = "John Smith is a good boy. His SSN is 123-45-6789 and email is john.smith@example.com. What should he do to avoid getting used by others?"
-#     # Like his name is getting used every, dragging through dirt of various programs
+    query = "John Smith is a good boy. His SSN is 123-45-6789 and email is john.smith@example.com. What should he do to avoid getting used by others?"
+    # Like his name is getting used every, dragging through dirt of various programs
 
-#     print(f"\nQuery: {query}")
+    print(f"\nQuery: {query}")
 
-#     response = await client.process_query(query)
+    response = await client.process_query(query)
 
-#     print(f"\nResponse: {response}")
+    print(f"\nResponse: {response}")
 
-#     await client.cleanup()
+    await client.cleanup()
 
 
 
-# if __name__ == "__main__":
-#     asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
 
-#     import sys
+    import sys
 
-#     sys.exit(0) 
+    sys.exit(0) 
